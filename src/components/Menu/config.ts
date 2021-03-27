a98f961e1028a7a2d35727bb512f24ca2ab226c7
Douglas Poma <douglaspoma@yahoo.com>
2021-03-26 22:57:35 -0300

    feat: all changes to btcoin operation

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,53 @@
+import { MenuEntry } from '@saltswap/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://btcoin.finance/'
+  },
+  {
+    label: 'Exchange',
+    href: '/swap',
+    icon: "ExchangeIcon"
+  },
+  {
+    label: 'Liquidity',
+    href: '/pool',
+    icon: 'LiquidityIcon'
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://btcoin.finance/#/farms'
+  },
+  {
+    label: 'Dens',
+    icon: 'PoolIcon',
+    href: 'https://btcoin.finance/#/pools'
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x71783d77d6019560770f7ce059a9d7294e1c4c70',
+      },
+      {
+        label: 'Binance Smart Chain (BSC)',
+        href: 'https://bscscan.com/token/0x71783d77d6019560770f7ce059a9d7294e1c4c70',
+      },
+      {
+        label: 'Token Info BSC',
+        href: 'https://bscscan.com/address/0xf328c01b7b895fe17e573dbaf739d9c25fcde210',
+      },
+      {
+        label: 'Exchange Swap BUSD x BTCOIN',
+        href: 'https://btcoin.finance/exchange/#/swap?outputCurrency=0x71783d77d6019560770f7ce059a9d7294e1c4c70&inputCurrency=BUSD',
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/constants/saltPriceContracts.ts
@@ -0,0 +1,12 @@
+const priceContracts: { saltAddress: string, busdAddress: string, lpAddress: string } = {
+  saltAddress: '0x71783d77d6019560770f7ce059a9d7294e1c4c70',
+  // saltAddress: '0x71783d77d6019560770f7ce059a9d7294e1c4c70',
+  // saltAddress: '0x2849b1aE7E04A3D9Bc288673A92477CF63F28aF4',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xf328C01B7b895FE17E573dbAF739d9c25fcdE210'
+  // lpAddress: '0xf328C01B7b895FE17E573dbAF739d9c25fcdE210'
+  // lpAddress: '0x6596f770786915556C47E301cC8290aa14288d99'
+  // lpAddress: '0x71783d77d6019560770f7ce059a9d7294e1c4c70'
+}
+
+export default priceContracts
