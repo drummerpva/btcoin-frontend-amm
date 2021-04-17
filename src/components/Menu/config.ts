8322481ca8124ae229df1e740d7bf9815e2f25c3
Douglas Poma <douglaspoma@yahoo.com>
2021-04-17 18:50:19 -0300

    feat: add menuitems

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -26,24 +26,28 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: 'https://btcoin.finance/#/pools'
   },
-  {
+   {
     label: 'Info',
     icon: 'InfoIcon',
     items: [
       {
-        label: 'GitHub',
-        href: 'https://github.com/drummerpva',
+        label: 'About',
+        href: 'https://btcoin.finance/btcoin.pdf',
       },
       {
         label: 'PancakeSwap',
         href: 'https://pancakeswap.info/token/0x71783d77d6019560770f7ce059a9d7294e1c4c70',
       },
       {
-        label: 'Binance Smart Chain (BSC)',
+        label: 'Binance Smart Chain Token',
         href: 'https://bscscan.com/token/0x71783d77d6019560770f7ce059a9d7294e1c4c70',
       },
       {
-        label: 'Token Info BSC',
+        label: 'Binance Smart Chain Address',
+        href: 'https://bscscan.com/address/0x71783d77d6019560770f7ce059a9d7294e1c4c70',
+      },
+      {
+        label: 'Binance Smart Chain Address',
         href: 'https://bscscan.com/address/0xf328c01b7b895fe17e573dbaf739d9c25fcde210',
       },
       {
@@ -51,6 +55,50 @@ const config: MenuEntry[] = [
         href: 'https://btcoin.finance/exchange/#/swap?outputCurrency=0x71783d77d6019560770f7ce059a9d7294e1c4c70&inputCurrency=BUSD',
       },
     ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items:[
+      {
+        label: 'GitHub',
+        href: 'https://github.com/drummerpva',
+      },
+      {
+        label: 'Astrotools.io',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0xf328C01B7b895FE17E573dbAF739d9c25fcdE210',
+      },
+    ],
+  },
+  {
+    label: 'Social Media',
+    icon: 'IfoIcon',
+    items:[
+      {
+        label: 'Telegram',
+        href: 'https://t.me/joinchat/nm6SkGwKU8I2Njlh',
+      },
+      {
+        label: 'Twitter',
+        href: 'https://twitter.com/btcoin14',
+      },
+      {
+        label: 'Youtube',
+        href: 'https://www.youtube.com/channel/UChznt9taplBh0cuhzA4I7AA',
+      },
+      {
+        label: 'Medium',
+        href: 'https://btcoin-finance.medium.com',
+      },
+    ],
+  },{
+    label: 'Whitepaper',
+    icon: 'TicketIcon',
+    href: 'https://btcoin.finance/btcoin.pdf',
+  },{
+    label: 'Audit',
+    icon: 'AuditIcon',
+    href: 'https://github.com/drummerpva/btcoin-audit',
   },
 ]
 
